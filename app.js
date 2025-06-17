6eb97b2268c45caca998f5a9a0c37d50658eade5
Fvazquez <foapgenerico@365.stucom.com>
2025-06-17 16:28:29 +0200

    stucom

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
 const express = require('express');
 const app = express();
 
+process.loadEnvFile();
+const PORT = process.argv[2] || process.env.PORT || 9999
 
+app.get('/', (req, res) => {
+    res.send('<h1>Estamos en la pagina inicial o home</h1>');
+})
+
+app.listen(PORT, () => {
+    console.log(`Servidor funcionando en http://localhost:${PORT}`);
+} )
