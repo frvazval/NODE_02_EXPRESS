73852260fdb7c2db089aaef67f60230c45ed8198
Fvazquez <foapgenerico@365.stucom.com>
2025-06-17 18:10:49 +0200

    stucom

--- public/js/404.js
+++ public/js/404.js
@@ -1,6 +1,13 @@
 function errorClick() {
     const h1 = document.querySelector('h1');
     if (h1) {
-        h1.style.color = 'red'; // Cambia 'red' por el color que prefieras
+        // Genera un color hexadecimal aleatorio
+        const randomColor = '#' + Math.floor(Math.random()*16777215).toString(16).padStart(6, '0');
+        h1.style.color = randomColor;
     }
+}
+
+const h1 = document.querySelector('h1');
+if (h1) {
+    h1.onclick = errorClick;
 }
